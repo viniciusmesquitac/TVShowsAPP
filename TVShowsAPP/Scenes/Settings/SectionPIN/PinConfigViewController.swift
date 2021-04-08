9457b5f9bc3b31deb84076318f563108b34f6c0c
viniciusmesquita <=>
2021-04-08 18:23:56 -0300

    Add pin textfield component

--- TVShowsAPP/Scenes/Settings/SectionPIN/PinConfigViewController.swift
+++ TVShowsAPP/Scenes/Settings/SectionPIN/PinConfigViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class PinConfigViewController: UIViewController {
+    let authTextField = AuthenticationTextField()
+    let titleInformationAuthLabel = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(
             barButtonSystemItem: .cancel, target: self, action: #selector(didTapCancel))
+        self.navigationItem.rightBarButtonItem?.tintColor = Stylesheet.Color.primaryColor
         self.view.backgroundColor = Stylesheet.Color.backgroundColor
+        setupAuthTextField()
+        setupTitleInformationAuthLabel()
+    }
+
+    func setupAuthTextField() {
+        view.addSubview(authTextField)
+        authTextField.configure()
+        authTextField.snp.makeConstraints { make in
+            make.centerY.equalToSuperview().offset(-12)
+            make.centerX.equalToSuperview()
+            make.height.equalTo(authTextField.height)
+            make.width.equalTo(authTextField.width)
+        }
+    }
+
+    func setupTitleInformationAuthLabel() {
+        view.addSubview(titleInformationAuthLabel)
+        titleInformationAuthLabel.textAlignment = .center
+        titleInformationAuthLabel.text = "Please write your pin code"
+        titleInformationAuthLabel.font = Stylesheet.Font.boldOfSize16
+        titleInformationAuthLabel.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(24)
+            make.trailing.equalToSuperview().offset(-24)
+            make.bottom.equalTo(authTextField).offset(-76)
+        }
     }
 
     @objc func didTapCancel() {
